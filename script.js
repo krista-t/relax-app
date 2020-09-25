e07c139c338a926ca3de59d583cb15eb248cda09
[kristina-tomicic] <[kris29h5@stud.kea.dk]>
2020-09-25 15:30:35 +0200

    dynamic part added

---
+++ script.js
@@ -0,0 +1,31 @@
+//GLOBAL VARIABLES
+
+const container = document.querySelector("#container");
+const text = document.querySelector("#text");
+const totalTime = 9000;
+const inhaleTime = (totalTime / 4.5) * 2;
+
+
+
+//one function to do it all
+function breathe() {
+
+//set text and animation for inhale 
+text.innerHTML = "INHALE"
+container.className = "grow"  
+
+//set text and animation, and time  for exhale
+setTimeout(() => {
+
+text.innerHTML = "EXHALE"
+container.className = "shrink"
+  
+}, inhaleTime);
+
+}
+
+
+breathe();
+
+setInterval(breathe, totalTime);
+
