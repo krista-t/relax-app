3cc2c0f7b59d5d02b6c00ce21855a4c3658b21b6
krista <krista.tomicic@gmail.com>
2021-04-10 17:12:36 +0200

    new bckg

--- script.js
+++ script.js
@@ -3,28 +3,20 @@
 const container = document.querySelector("#container");
 const text = document.querySelector("#text");
 const totalTime = 9000;
-const inhaleTime = (totalTime / 4.5) * 2;
-
-
+const inhaleTime = (totalTime / 4) * 2;
 
 //one function to do it all
 function breathe() {
-
-  //set text and animation for inhale 
-  text.innerHTML = "INHALE"
-  container.className = "grow"
+  //set text and animation for inhale
+  text.innerHTML = "INHALE";
+  container.className = "grow";
 
   //set text and animation, and time  for exhale
   setTimeout(() => {
-
-    text.innerHTML = "EXHALE"
-    container.className = "shrink"
-
+    text.innerHTML = "EXHALE";
+    container.className = "shrink";
   }, inhaleTime);
-
 }
-
-
 
 breathe();
 setInterval(breathe, totalTime);
